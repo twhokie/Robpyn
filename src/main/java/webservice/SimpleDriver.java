fde304312329a59b08a523050c4717956435ef65
tnaggs <tnaggs@gmail.com>
2016-01-22 11:41:17 -0500

    further mavenizing

---
+++ src/main/java/webservice/SimpleDriver.java
@@ -0,0 +1,17 @@
+package webservice;
+
+import org.python.util.PythonInterpreter;
+import org.python.core.*;
+
+
+public class SimpleDriver {
+	
+	   public static void main(String []args) throws PyException
+	   {
+	       PythonInterpreter pi = new PythonInterpreter();
+	       pi.set("integer", new PyInteger(42));
+	       pi.exec("square = integer*integer");
+	       PyInteger square = (PyInteger)pi.get("square");
+	       System.out.println("square: " + square.asInt());
+	   }
+}
